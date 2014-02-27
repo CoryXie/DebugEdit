3b5e43b8933c56b707bb3c28f404bc04a20ca191
CoryXie <cory.xie@gmail.com>
2014-02-28 00:24:33 +0800

    Merge latest DWARF4 support.

--- debugedit.c
+++ debugedit.c
@@ -40,6 +40,10 @@
 #include "hashtab.h"
 
 #define DW_TAG_partial_unit 0x3c
+#define DW_FORM_sec_offset 0x17
+#define DW_FORM_exprloc 0x18
+#define DW_FORM_flag_present 0x19
+#define DW_FORM_ref_sig8 0x20
 
 char *base_dir = NULL;
 char *dest_dir = NULL;
@@ -215,6 +219,9 @@ static struct
 #define DEBUG_STR	8
 #define DEBUG_FRAME	9
 #define DEBUG_RANGES	10
+#define DEBUG_TYPES	11
+#define DEBUG_MACRO	12
+#define DEBUG_GDB_SCRIPT	13
         { ".debug_info", NULL, NULL, 0, 0, 0 },
         { ".debug_abbrev", NULL, NULL, 0, 0, 0 },
         { ".debug_line", NULL, NULL, 0, 0, 0 },
@@ -226,6 +233,9 @@ static struct
         { ".debug_str", NULL, NULL, 0, 0, 0 },
         { ".debug_frame", NULL, NULL, 0, 0, 0 },
         { ".debug_ranges", NULL, NULL, 0, 0, 0 },
+        { ".debug_types", NULL, NULL, 0, 0, 0 },
+        { ".debug_macro", NULL, NULL, 0, 0, 0 },
+        { ".debug_gdb_scripts", NULL, NULL, 0, 0, 0 },
         { NULL, NULL, NULL, 0, 0, 0 }
     };
 
@@ -318,7 +328,8 @@ no_memory:
                     goto no_memory;
                 }
             form = read_uleb128 (ptr);
-            if (form == 2 || form > DW_FORM_indirect)
+            if (form == 2
+                    || (form > DW_FORM_flag_present && form != DW_FORM_ref_sig8))
                 {
                 error (0, 0, "%s: Unknown DWARF DW_FORM_%d", dso->filename, form);
                 htab_delete (h);
@@ -470,6 +481,10 @@ edit_dwarf2_line (DSO *dso, uint32_t off, char *comp_dir, int phase)
     if (phase != 0)
         return 0;
 
+    /* XXX: RhBug:929365, should we error out instead of ignoring? */
+    if (ptr == NULL)
+        return 0;
+
     ptr += off;
 
     endcu = ptr + 4;
@@ -488,7 +503,7 @@ edit_dwarf2_line (DSO *dso, uint32_t off, char *comp_dir, int phase)
         }
 
     value = read_16 (ptr);
-    if (value != 2 && value != 3)
+    if (value != 2 && value != 3 && value != 4)
         {
         error (0, 0, "%s: DWARF version %d unhandled", dso->filename,
                value);
@@ -504,8 +519,8 @@ edit_dwarf2_line (DSO *dso, uint32_t off, char *comp_dir, int phase)
         return 1;
         }
 
-    opcode_base = ptr[4];
-    ptr = dir = ptr + 4 + opcode_base;
+    opcode_base = ptr[4 + (value >= 4)];
+    ptr = dir = ptr + 4 + (value >= 4) + opcode_base;
 
     /* dir table: */
     value = 1;
@@ -732,7 +747,8 @@ edit_attributes (DSO *dso, unsigned char *ptr, struct abbrev_tag *t, int phase)
             {
             if (t->attr[i].attr == DW_AT_stmt_list)
                 {
-                if (form == DW_FORM_data4)
+                if (form == DW_FORM_data4
+                        || form == DW_FORM_sec_offset)
                     {
                     list_offs = do_read_32_relocated (ptr);
                     found_list_offs = 1;
@@ -834,6 +850,8 @@ edit_attributes (DSO *dso, unsigned char *ptr, struct abbrev_tag *t, int phase)
                     else
                         ptr += 4;
                     break;
+                case DW_FORM_flag_present:
+                    break;
                 case DW_FORM_addr:
                     ptr += ptr_size;
                     break;
@@ -848,10 +866,12 @@ edit_attributes (DSO *dso, unsigned char *ptr, struct abbrev_tag *t, int phase)
                     break;
                 case DW_FORM_ref4:
                 case DW_FORM_data4:
+                case DW_FORM_sec_offset:
                     ptr += 4;
                     break;
                 case DW_FORM_ref8:
                 case DW_FORM_data8:
+                case DW_FORM_ref_sig8:
                     ptr += 8;
                     break;
                 case DW_FORM_sdata:
@@ -880,6 +900,7 @@ edit_attributes (DSO *dso, unsigned char *ptr, struct abbrev_tag *t, int phase)
                     form = DW_FORM_block1;
                     break;
                 case DW_FORM_block:
+                case DW_FORM_exprloc:
                     len = read_uleb128 (ptr);
                     form = DW_FORM_block1;
                     assert (len < UINT_MAX);
@@ -1179,7 +1200,7 @@ fail:
                     }
 
                 cu_version = read_16 (ptr);
-                if (cu_version != 2 && cu_version != 3)
+                if (cu_version != 2 && cu_version != 3 && cu_version != 4)
                     {
                     error (0, 0, "%s: DWARF version %d unhandled", dso->filename,
                            cu_version);
@@ -1445,6 +1466,12 @@ main (int argc, char *argv[])
         switch (dso->shdr[i].sh_type)
             {
             case SHT_PROGBITS:
+                /* TODO: Handle stabs */
+                if (strcmp (name, ".stab") == 0)
+                    {
+                    fprintf (stderr, "Stabs debuginfo not supported: %s\n", file);
+                    break;
+                    }
                 if (strcmp (name, ".debug_info") == 0)
                     edit_dwarf2 (dso);
 
